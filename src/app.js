805a72359780ebdf503eab45244b7e91c4197012
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-16 11:53:08 -0700

    updates

--- src/app.js
+++ src/app.js
@@ -20,7 +20,6 @@ quizButton.onclick = function() {
     
     // example of what an empty string is
     // eslint-disable-next-line no-unused-vars
-    const emptyString = '';
 
     let name = '';
     
@@ -37,18 +36,39 @@ quizButton.onclick = function() {
     // name += lastName;
 
     // get the text response from the user
-    const child = prompt('Does Dylan have a child to return home to? (Y/N)?');
+    const child = prompt('Does Dylan have a child to return home to? (Y/N)');
     
     // convert to boolean
-    const correct = isYes(child);
+    let correct = isYes(child);
 
+    let correctAnswers = 0;
+    if(correct === true) {
+        (correctAnswers += 1);
+    } 
+
+    // get the text response from the user
+    const teeterMetal = prompt('Dylan has many interesting skills. Is Dylan the first black metal teeter board artist? (Y/N)');
+    
+    // convert to boolean
+    correct = isYes(teeterMetal);
+    if(correct === true) {
+        (correctAnswers += 1);
+    // get the text response from the user
+    const catName = prompt('Does Dylan have a cat named Lovecraft? (Y/N)');
+    
+    // convert to boolean
+    correct = isYes(catName);
+    if(correct === true) {
+        (correctAnswers += 1);
+
+    //Responses to quiz
     let response = 'Thank you ' + name;
 
-    if(correct) {
-        response += ' you saved Dylan! They can return to their family and live their life again!';
+    if(correctAnswers === 3) {
+        response += ', you got' + correctAnswers + ' out of 3 questions correct and have saved Dylan! They can return to their family and live their life again!';
     }
     else {
-        response += ' for trying to save Dylan. Train harder and perhaps you can rescue them in the future.';
+        response += ' for trying to save Dylan. You got' + correctAnswers + ' out of 3 questions correct. Please go back to train and perhaps you can rescue them in the future.';
     }
 
     result.textContent = response;
