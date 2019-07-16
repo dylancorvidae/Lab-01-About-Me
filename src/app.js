9a10c2e3b66ca44ee2addfec548629deb07b8102
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-16 12:06:57 -0700

    working quiz!

--- src/app.js
+++ src/app.js
@@ -39,36 +39,34 @@ quizButton.onclick = function() {
     const child = prompt('Does Dylan have a child to return home to? (Y/N)');
     
     // convert to boolean
-    let correct = isYes(child);
 
     let correctAnswers = 0;
-    if(correct === true) {
-        (correctAnswers += 1);
+    if(isYes(child) === true) {
+        correctAnswers += 1;
     } 
 
     // get the text response from the user
     const teeterMetal = prompt('Dylan has many interesting skills. Is Dylan the first black metal teeter board artist? (Y/N)');
     
     // convert to boolean
-    correct = isYes(teeterMetal);
-    if(correct === true) {
-        (correctAnswers += 1);
+    if(isYes(teeterMetal) === false) {
+        correctAnswers += 1;
+    }
     // get the text response from the user
     const catName = prompt('Does Dylan have a cat named Lovecraft? (Y/N)');
     
     // convert to boolean
-    correct = isYes(catName);
-    if(correct === true) {
-        (correctAnswers += 1);
-
+    if(isYes(catName) === false) {
+        correctAnswers += 1;
+    }
     //Responses to quiz
     let response = 'Thank you ' + name;
 
     if(correctAnswers === 3) {
-        response += ', you got' + correctAnswers + ' out of 3 questions correct and have saved Dylan! They can return to their family and live their life again!';
+        response += ', you got ' + correctAnswers + ' out of 3 questions correct and have saved Dylan! They can return to their family and live their life again!';
     }
     else {
-        response += ' for trying to save Dylan. You got' + correctAnswers + ' out of 3 questions correct. Please go back to train and perhaps you can rescue them in the future.';
+        response += ' for trying to save Dylan. You got ' + correctAnswers + ' out of 3 questions correct. Please go back to train and perhaps you can rescue them in the future.';
     }
 
     result.textContent = response;
