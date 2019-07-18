a8c681f9d7858fd74965bdbfd82f6aa651cf0764
Dylan Corvidae <dylancorvidae@gmail.com>
2019-07-18 13:59:52 -0700

    realized I didn't actually finish, I just got so excited things were changing on my page but I forgot to add ties in and my wins were evaluated to be if the computer and user both choose the same thing.

--- src/rpsapp.js
+++ src/rpsapp.js
@@ -8,10 +8,12 @@ const message = document.getElementById('message');
 const opponentThrows = document.getElementById('opponent-throws');
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
+//const tieCount = document.getElementById('tie-count');
 
 // Initialization (setup, state variables)
 let wins = 0;
 let losses = 0;
+//let ties = 0;
 
 // Event Handlers
 doItButton.addEventListener('click', () => {
@@ -22,11 +24,43 @@ doItButton.addEventListener('click', () => {
     const src = 'assets/weapon/' + rps + '.png';
     resultImg.src = src;
 
-    const sideButton = document.querySelector('input:checked');
+    const throwButton = document.querySelector('input:checked');
 
-    const won = sideButton.value === rps;
+    const won = throwButton.value === rps;
 
 //Show Result & Tally Score
+/* let result =
+
+if(throwButton.value === 'rock' && rps === "scissors" ||
+throwButton.value === 'paper' && rps === 'rock' ||
+throwButton.value === 'scissors' && rps === 'paper')
+return 'win';
+
+else (throwButton.value === 'rock' && rps === "paper" ||
+throwButton.value === 'paper' && rps === 'scissors' ||
+throwButton.value === 'scissors' && rps === 'rock')
+return 'loss'
+
+else if(throwButton.value === 'rock' && rps === "rock" ||
+throwButton.value === 'paper' && rps === 'paper' ||
+throwButton.value === 'scissors' && rps === 'scissors')
+return 'tie'
+
+
+if(won) {
+        message.textContent = 'Congratulations! You Won! Do you want to try your luck again?';
+        wins++;
+        winCount.textContent = wins;
+    } else {
+        message.textContent = 'You Lost! Want to try again?';
+        losses++;
+        lossCount.textContent = losses;
+    } else if {
+        message.textContent = 'It was a tie! Would you like to try again?';
+        ties++;
+        tieCount.textContent = ties;
+*/
+
     if(won) {
         message.textContent = 'Congratulations! You Won! Do you want to try your luck again?';
         wins++;
