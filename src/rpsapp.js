714ff0871ed9e291ec39af389df8ff771b03a88f
Kamran Ali <kgali21@gmail.com>
2019-07-18 17:17:28 -0700

    parter review fixed javascriptin rps

--- src/rpsapp.js
+++ src/rpsapp.js
@@ -8,12 +8,12 @@ const message = document.getElementById('message');
 const opponentThrows = document.getElementById('opponent-throws');
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
-//const tieCount = document.getElementById('tie-count');
+const tieCount = document.getElementById('tie-count');
 
 // Initialization (setup, state variables)
 let wins = 0;
 let losses = 0;
-//let ties = 0;
+let ties = 0;
 
 // Event Handlers
 doItButton.addEventListener('click', () => {
@@ -26,48 +26,45 @@ doItButton.addEventListener('click', () => {
 
     const throwButton = document.querySelector('input:checked');
 
-    const won = throwButton.value === rps;
+    let winCondition;
+    //Show Result & Tally Score
+    winCondition = compareThrows(throwButton, rps, winCondition);
+    resultTally(winCondition);
+});
 
-//Show Result & Tally Score
-/* let result =
+function compareThrows(throwButton, rps, winCondition) {
+    if(throwButton.value === 'rock' && rps === 'scissors' ||
+        throwButton.value === 'paper' && rps === 'rock' ||
+        throwButton.value === 'scissors' && rps === 'paper') {
+        winCondition = 1;
+    }
+    else if(throwButton.value === 'rock' && rps === 'paper' ||
+        throwButton.value === 'paper' && rps === 'scissors' ||
+        throwButton.value === 'scissors' && rps === 'rock') {
+        winCondition = -1;
+    }
+    else if(throwButton.value === 'rock' && rps === 'rock' ||
+        throwButton.value === 'paper' && rps === 'paper' ||
+        throwButton.value === 'scissors' && rps === 'scissors') {
+        winCondition = 0;
+    }
+    return winCondition;
+}
 
-if(throwButton.value === 'rock' && rps === "scissors" ||
-throwButton.value === 'paper' && rps === 'rock' ||
-throwButton.value === 'scissors' && rps === 'paper')
-return 'win';
-
-else (throwButton.value === 'rock' && rps === "paper" ||
-throwButton.value === 'paper' && rps === 'scissors' ||
-throwButton.value === 'scissors' && rps === 'rock')
-return 'loss'
-
-else if(throwButton.value === 'rock' && rps === "rock" ||
-throwButton.value === 'paper' && rps === 'paper' ||
-throwButton.value === 'scissors' && rps === 'scissors')
-return 'tie'
-
-
-if(won) {
+function resultTally(winCondition) {
+    if(winCondition === 1) {
         message.textContent = 'Congratulations! You Won! Do you want to try your luck again?';
         wins++;
         winCount.textContent = wins;
-    } else {
-        message.textContent = 'You Lost! Want to try again?';
-        losses++;
-        lossCount.textContent = losses;
-    } else if {
-        message.textContent = 'It was a tie! Would you like to try again?';
-        ties++;
-        tieCount.textContent = ties;
-*/
-
-    if(won) {
-        message.textContent = 'Congratulations! You Won! Do you want to try your luck again?';
-        wins++;
-        winCount.textContent = wins;
-    } else {
+    }
+    else if(winCondition === -1) {
         message.textContent = 'You Lost! Want to try again?';
         losses++;
         lossCount.textContent = losses;
     }
-});
+    else if(winCondition === 0) {
+        message.textContent = 'It was a tie! Would you like to try again?';
+        ties++;
+        tieCount.textContent = ties;
+    }
+}
